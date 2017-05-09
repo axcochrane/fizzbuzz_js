1f43c449be1e59cde989c05ccef4176ba628123b
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-05-09 14:46:38 +0100

    new test writter. checks ifDivisibleByThree

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,9 @@
+describe('Fizzbuzz',function(){
+    var fizzbuzz;
+        describe('knows when a number is',function(){
+           it('divisible by 3', function(){
+               fizzbuzz = new Fizzbuzz();
+                    expect(fizzbuzz.isDivisibleByThree(6)).toBe(true);             
+           }); 
+        });
+});
